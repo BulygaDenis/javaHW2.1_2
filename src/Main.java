0e448e91519fc42424e063df641d11441f0e9495
Neposeda644 <denis_bulyga@mail.ru>
2020-09-29 21:50:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean registred = true;
+        int percent;
+        if (registred) {
+            percent = 1;
+        }
+        long account = 100;
+        long transfer = 100;
+        long bonus = transfer/100;
+        long limit = 1000;
+        if (transfer < limit) {
+            bonus = 0;
+        }
+        long amount = account + transfer + bonus;
+        System.out.println(amount);
+        }
+}
