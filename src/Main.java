66d376721a4d4aa5caab96c4748fcb5b8889f0b2
Neposeda644 <denis_bulyga@mail.ru>
2020-09-30 09:36:50 +0300

    format UPD

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,13 @@ public class Main {
             percent = 1;
         }
         long account = 100;
-        long transfer = 100;
-        long bonus = transfer/100;
+        long transfer = 2100;
+        long bonus = transfer / 100;
         long limit = 1000;
         if (transfer < limit) {
             bonus = 0;
         }
         long amount = account + transfer + bonus;
         System.out.println(amount);
-        }
+    }
 }
